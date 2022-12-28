b73ea7f3a5107c08f990d0ef107cf042b151050a
AnkitaSMusale <ankitamusale4@gmail.com>
2022-12-28 22:45:27 +0530

    Passing data via props - hard code

--- src/App.js
+++ src/App.js
@@ -3,29 +3,44 @@ import './App.css';
 import ExpenseItem  from './components/ExpenseItem';
 
 function App() {
+  const expense = [
+    {id: '01', descr:'petrol',date : new Date(2022,12,28), price : 200 , expenditurelocation:'pune'},
+    {id: '02', descr:'pizza',date : new Date(2022,12,29), price : 300, expenditurelocation:'mumbai'},
+    {id: '03', descr:'flower',date : new Date(2022,12,30), price : 400, expenditurelocation:'bangalore'},
+    {id: '04',descr:'book',date : new Date(2022,12,31), price : 500, expenditurelocation:'solapur'}
+  ];
+
   return (
     <div>
       <h2>This is Ankita</h2>
       <p>Hello , Welcome</p>
       <h2>Expense Item !</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem 
+        descr = {expense[0].descr}
+        date = {expense[0].date}
+        price = {expense[0].price}
+        expenditurelocation = {expense[0].expenditurelocation}
+      ></ExpenseItem>
+      <ExpenseItem
+      descr = {expense[1].descr}
+      date = {expense[1].date}
+      price = {expense[1].price}
+      expenditurelocation = {expense[1].expenditurelocation}
+      ></ExpenseItem>
+      <ExpenseItem 
+        descr = {expense[2].descr}
+        date = {expense[2].date}
+        price = {expense[2].price}
+        expenditurelocation = {expense[2].expenditurelocation}
+      ></ExpenseItem>
+      <ExpenseItem 
+        descr = {expense[3].descr}
+        date = {expense[3].date}
+        price = {expense[3].price}
+        expenditurelocation = {expense[3].expenditurelocation}
+      ></ExpenseItem>
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(){
-    const expensedate = new Date(2022, 12, 28);
-    const descr = 'petrol';
-    const expenditurelocation = 'pune';
-    const price = 200;
+function ExpenseItem(props){
+    // const expensedate = new Date(2022, 12, 28);
+    // const descr = 'petrol';
+    // const expenditurelocation = 'pune';
+    // const price = 200;
     return (
         <div className='expenses'>
-            <div className='expense-item'><h3>{expensedate.toISOString()}</h3></div>
-            <div className='expense-item_description'><h3>{descr}</h3></div>
-            <div className='expense-item_location'><h3>{expenditurelocation}</h3></div>
-            <div className='expense-item_price'><h3>${price}</h3></div>
+            <div className='expense-item'><h3>{props.date.toISOString()}</h3></div>
+            <div className='expense-item_description'><h3>{props.descr}</h3></div>
+            <div className='expense-item_location'><h3>{props.expenditurelocation}</h3></div>
+            <div className='expense-item_price'><h3>${props.price}</h3></div>
         </div>
         )
 }

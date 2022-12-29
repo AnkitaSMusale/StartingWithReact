a004e0f312a0442985ad7f2f01ed5dbebe7a7946
AnkitaSMusale <ankitamusale4@gmail.com>
2022-12-30 00:22:53 +0530

    Adding Eventlistners(buttons) - static

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -5,10 +5,25 @@ import ExpenseDetail from './ExpenseDetail';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) =>{
+    const clickHandler = () => {
+        console.log('clicked...')
+    }
+
+    const DeleteExpense = () =>{
+            // let item = this.state.data.filter()
+            // let item = props.remove(e.ExpenseItem)
+            // props.remove(item.id)
+            console.log('Expense deleted successfully...')
+    }
+
     return (
         <Card className='expenses'>
-            <ExpenseDate date={props.date}></ExpenseDate>    
-            <ExpenseDetail descr={props.descr} expenditurelocation={props.expenditurelocation} price={props.price}></ExpenseDetail> 
+            <ExpenseDate date={props.date}></ExpenseDate>
+            <h2>{props.descr}</h2> 
+            {/* expenditurelocation={props.expenditurelocation}  */}
+            <ExpenseDetail price={props.price}></ExpenseDetail> 
+            <button onClick={clickHandler}>Change Desc</button>
+            <button onClick={DeleteExpense}>Delete</button>
         </Card>
         )
 }

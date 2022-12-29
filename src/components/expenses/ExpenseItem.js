1e4da2fd67a52a44bd3c5901fbac6a9e2a623ddf
AnkitaSMusale <ankitamusale4@gmail.com>
2022-12-30 01:53:31 +0530

    useState() to change price

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -9,28 +9,33 @@ import Card from '../UI/Card';
 const ExpenseItem = (props) =>{
 
     const [descr, setTitle] = useState(props.descr);  //destructing
-    // let descr = props.descr;
     const clickHandler = () => {
         setTitle('Updated')
-        // descr = 'Updated';
         console.log(descr)
     }
 
-    const DeleteExpense = () =>{
-            // let item = this.state.data.filter()
-            // let item = props.remove(e.ExpenseItem)
-            // props.remove(item.id)
-            console.log('Expense deleted successfully...')
+    const [price, setPrice] = useState(props.price);  //destructing
+    const priceChange = () => {
+        setPrice('100')
+        console.log(price)
     }
+
+    // const DeleteExpense = () =>{
+    //         // let item = this.state.data.filter()
+    //         // let item = props.remove(e.ExpenseItem)
+    //         // props.remove(item.id)
+    //         console.log('Expense deleted successfully...')
+    // }
 
     return (
         <Card className='expenses'>
             <ExpenseDate date={props.date}></ExpenseDate>
             <h2>{descr}</h2> 
             {/* expenditurelocation={props.expenditurelocation}  */}
-            <ExpenseDetail price={props.price}></ExpenseDetail> 
+            <ExpenseDetail price={price}></ExpenseDetail> 
             <button onClick={clickHandler}>Change Desc</button>
-            <button onClick={DeleteExpense}>Delete</button>
+            {/* <button onClick={DeleteExpense}>Delete</button> */}
+            <button onClick={priceChange}>Change Price</button>
         </Card>
         )
 }

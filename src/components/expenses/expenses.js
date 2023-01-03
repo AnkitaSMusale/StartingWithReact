c074452238ebea4052446d88b791d3834964534e
AnkitaSMusale <ankitamusale4@gmail.com>
2023-01-04 00:27:05 +0530

    Adding conditional statements - &&

--- src/components/expenses/expenses.js
+++ src/components/expenses/expenses.js
@@ -19,47 +19,30 @@ const Expenses = (props) => {
     return(
         <div>
           <ExpensesFilter selectedYear={filteredYear} onYearSelect={yearSelectHandler} />
-          {FilteredExpense.map((expense) => (
+          {FilteredExpense.length === 0 && <p>No data found !!</p>}
+          {FilteredExpense.length > 0 && 
+          FilteredExpense.map((expense) => (
             <ExpenseItem 
             key={expense.id}
             descr = {expense.descr}
             date = {expense.date}
             price = {expense.price}
             />
-          ))
+          ))}
+
+          {FilteredExpense.length ===1 && 
+          FilteredExpense.map((expense) => (
+            <ExpenseItem 
+            key={expense.id}
+            descr = {expense.descr}
+            date = {expense.date}
+            price = {expense.price}
+            />
+          ))  &&
+          <p>Only Single Item Found ! Please add more items </p>
           }
-          {/* <ExpenseItem 
-            descr = {props.items[0].descr}
-            date = {props.items[0].date}
-            price = {props.items[0].price}
-            expenditurelocation = {props.items[0].expenditurelocation}
-          ></ExpenseItem>
-      <ExpenseItem
-      descr = {props.items[1].descr}
-      date = {props.items[1].date}
-      price = {props.items[1].price}
-      expenditurelocation = {props.items[1].expenditurelocation}
-      ></ExpenseItem>
-      <ExpenseItem 
-        descr = {props.items[2].descr}
-        date = {props.items[2].date}
-        price = {props.items[2].price}
-        expenditurelocation = {props.items[2].expenditurelocation}
-      ></ExpenseItem>
-      <ExpenseItem 
-        descr = {props.items[3].descr}
-        date = {props.items[3].date}
-        price = {props.items[3].price}
-        expenditurelocation = {props.items[3].expenditurelocation}
-    ></ExpenseItem> */}
-    {/* for(let i=0; i<expense.length; i++){
-        <ExpenseItem 
-        descr = {expense[i].descr}
-        date = {expense[i].date}
-        price = {expense[i].price}
-        expenditurelocation = {expense[i].expenditurelocation}
-        ></ExpenseItem>
-      } */}
+
+        
         </div>
     )
 
